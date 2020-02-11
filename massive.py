eeba3522d1c4fdc827c0bd5ededfc80b907f394a
Nikita111222 <58566365+Nikita111222@users.noreply.github.com>
2020-02-11 19:19:03 +0300

    Update massive.py

--- massive.py
+++ massive.py
@@ -1,10 +1,11 @@
 from random import randint 
+N = 10
 a = []
-for k in range(10):
+for k in range(N):
     a.append(randint(1, 100))
 print(a)
-for h in range(9):
-    for i in range(9-h):
+for h in range(N-1):
+    for i in range(N-1-h):
         if a[i] > a[i + 1]:
             tmp = a[i + 1]
             a[i + 1] = a[i]
