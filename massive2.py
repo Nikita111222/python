4b0735aad8f8cf41205c28987aba7a6485e8a23e
Nikita111222 <58566365+Nikita111222@users.noreply.github.com>
2020-02-18 19:27:07 +0300

    Create massive2.py

---
+++ massive2.py
@@ -0,0 +1,26 @@
+from random import randint
+
+N = 20
+a = []
+b = [] 
+M = 1
+tmp = 0
+for i in range(N):
+    a.append(randint(1, 100))
+print(a)
+
+for i in range(N-1):
+    if a[i] < a[i+1]:
+        M = M + 1
+        tmp = M
+    else:
+        b.append(tmp)
+        M = 0 
+K = len(b)
+print(b)    
+
+l = 0 
+for i in range(K):
+    if l < b[i]:
+        l = b[i] 
+print(l)
